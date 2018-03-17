4e09b6cc4bb9092fc0bb71cb666006a27b661f75
benAkehurst <benakehurst@hotmail.com>
2018-03-17 17:30:22 +0200

    get 12 recent images from instagram

--- src/app/Components/InstagramComponent/instagram.component.ts
+++ src/app/Components/InstagramComponent/instagram.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../Services/data.service';
+import { Http } from '@angular/http/src/http';
 
 @Component({
     selector: 'app-instagram',
@@ -9,11 +10,32 @@ import { DataService } from '../../Services/data.service';
 export class InstagramComponent implements OnInit {
 
     constructor(public dataService: DataService) { }
-
+    errors: any;
     instagram: String = 'Hello instagram';
+    twelveRecentImages: any[] = [];
+    urls: Array<any> = [];
 
     ngOnInit() {
-
+        this.getInstagramPhotos();
     }
 
+    public getInstagramPhotos() {
+        this.dataService.getInstagramPhotos().subscribe(res => {
+            this.twelveRecentImages = res.graphql.user.edge_owner_to_timeline_media.edges;
+            this.getRecentImageUrls();
+        },
+        error => {
+            this.errors = error;
+        });
+    }
+
+    public getRecentImageUrls() {
+        this.twelveRecentImages.forEach(i => {
+            this.urls.push(
+            {
+                'url' : i.node.display_url
+            });
+        });
+        console.log(this.urls);
+    }
 }
--- src/app/Services/data.service.ts
+++ src/app/Services/data.service.ts
@@ -4,6 +4,7 @@ import { Router, RouterModule, Routes } from '@angular/router';
 import { Response } from '@angular/http/src/static_response';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import keys from './../Models/FrontEndKeys';
 
 @Injectable()
 export class DataService {
@@ -11,4 +12,17 @@ export class DataService {
     private headers = new Headers({ 'Content-Type': 'application/json' });
 
     constructor(private http: Http) { }
+
+    //
+    // ─── INSTAGRAM ──────────────────────────────────────────────────────────────────
+    //
+    public getInstagramPhotos() {
+        return this.http
+            .get('https://www.instagram.com/thatbritisraeli/?__a=1')
+            .map(res => res.json());
+    }
+    //
+    // ──────────────────────────────────────────────────────────────── INSTAGRAM ─────
+    //
+
 }
