593e93e5900be2619122abcdddf439522cf90f98
benAkehurst <benakehurst@hotmail.com>
2018-03-20 07:09:01 +0200

    building projects section

--- src/app/Components/ProjectsComponent/projects.component.ts
+++ src/app/Components/ProjectsComponent/projects.component.ts
@@ -12,16 +12,72 @@ export class ProjectsComponent implements OnInit {
 
     projects: Array<any> = [
         {
-            name: 'test',
+            name: 'Munch',
             image: 'images/profilepic.jpg',
             description: 'A test',
-            techStack: 'AngularJS, NodeJS'
+            techStack: 'Angular, NodeJS',
+            githubLink: '',
+            liveDemoLink: ''
         },
         {
-            name: 'test',
+            name: 'Random Colors',
             image: 'images/profilepic.jpg',
             description: 'A test',
-            techStack: 'AngularJS, NodeJS'
+            techStack: 'HTML, CSS, Javascript',
+            githubLink: '',
+            liveDemoLink: ''
+        },
+        {
+            name: 'Playlistim',
+            image: 'images/profilepic.jpg',
+            description: 'A test',
+            techStack: 'AngularJS, NodeJS, MongoDB',
+            githubLink: '',
+            liveDemoLink: ''
+        },
+        {
+            name: 'Hanoi',
+            image: 'hanoi.jpg',
+            description: 'A test',
+            techStack: 'AngularJS',
+            githubLink: '',
+            liveDemoLink: ''
+        },
+        {
+            name: 'Gif Search',
+            image: 'images/profilepic.jpg',
+            description: 'A test',
+            techStack: 'AngularJS',
+            githubLink: '',
+            liveDemoLink: ''
+        },
+        {
+            name: 'Unique Password Generator',
+            image: 'images/profilepic.jpg',
+            description: 'A test',
+            techStack: 'AngularJS',
+            githubLink: '',
+            liveDemoLink: ''
+        }
+    ];
+    professionalWork: Array<any> = [
+        {
+            client: 'Company A',
+            theme : 'Cyber Security SaaS',
+            technology: 'AngularJS',
+            role: 'team member'
+        },
+        {
+            client: 'Maccabi Haifa FC',
+            theme: 'Website Re-design',
+            technology: 'CSS',
+            role: 'team member'
+        },
+        {
+            client: 'Company W',
+            theme: 'Socail Networking Forum',
+            technology: 'Angular, NodeJS, MongoDB, AWS',
+            role: 'team leader'
         }
     ];
 
