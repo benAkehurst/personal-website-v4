463270c3ad1ba0d6843bc7925fcdb699b926bc8a
benAkehurst <benakehurst@hotmail.com>
2018-03-22 20:54:30 +0000

    contact added social links

--- src/app/Components/ContactComponent/contact.component.ts
+++ src/app/Components/ContactComponent/contact.component.ts
@@ -16,4 +16,20 @@ export class ContactComponent implements OnInit {
 
     }
 
+    public goToGitHub() {
+        window.open('https://github.com/benAkehurst', '_blank').focus();
+    }
+
+    public goToInstagram() {
+        window.open('https://www.instagram.com/thatbritisraeli', '_blank').focus();
+    }
+
+    public goToYoutube() {
+        window.open('https://www.youtube.com/user/ClicheProductionsUK', '_blank').focus();
+    }
+
+    public goToLinkedin() {
+        window.open('https://www.linkedin.com/in/ben-akehurst/', '_blank').focus();
+    }
+
 }
