05cc909fa0a09b6175197c07d7a9de93fbd86777
benAkehurst <benakehurst@hotmail.com>
2018-07-26 22:42:18 +0300

    Added skills summery json

--- src/app/Components/DevelopmentComponent/development.component.ts
+++ src/app/Components/DevelopmentComponent/development.component.ts
@@ -7,13 +7,24 @@ import { DataService } from '../../Services/data.service';
     styleUrls: ['./development.component.scss']
 })
 export class DevelopmentComponent implements OnInit {
+  development: String = 'Hello Development';
+  skillsData: Object = {};
 
-    constructor(public dataService: DataService) { }
-
-    development: String = 'Hello Development';
-
-    ngOnInit() {
-
+  constructor(public dataService: DataService) {
+    if (!this.skillsData) {
+      this.skillsData = {};
     }
+  }
+
+  ngOnInit() {
+    this.getSkillsJSON();
+  }
+
+  public getSkillsJSON() {
+    this.dataService.getSkillsData()
+      .subscribe(res => {
+        this.skillsData = res;
+      });
+  }
 
 }
